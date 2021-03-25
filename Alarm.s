e8a4954358fc96e7160e6c1e26bfa886cfdbfbbc
urvij <urvij@DESKTOP-N8GA505>
2021-03-25 16:01:04 +0000

    trying to put Alarm in different file

--- Alarm.s
+++ Alarm.s
@@ -1,141 +1,155 @@
-#include <xc.inc>
+#include<xc.inc>
+
+global	alarm_hrs, alarm_min, alarm_sec, alarm_on, Check_Alarm, Display_Alarm_Time
     
-;global	check_alarm
-
-psect	Alarm_code, class=CODE
-check_alarm:
-;	movlw	0x00
-;	cpfseq	Alarm_buzz		;check if alarm_buzz has reached 0
-;	bra	decrement_alarm_buzz	;keep decrementing and buzzing if it hasn't
-	;bra	compare_alarm		;go to compare alarm and return to normal cycles if it has reached 0
-
-;decrement_alarm_buzz:
-	;decf	Alarm_buzz		;subroutine to keep decrementing alarm_buzz
-	;call	ALARM			;and buzzing
-	;return
+  psect	udata_acs  
+alarm_buzz: ds 1
+buzz_bit: ds	1
+    
+buzzer_counter_1: ds 1
+buzzer_counter_2: ds 1
+alarm_on:   ds 1
+   
+alarm_sec:	ds  1
+alarm_min:	ds  1
+alarm_hrs:	ds  1
+    
+    psect	Alarm_code, class=CODE
+alarm_setup:
+	bcf	alarm_on, 0, A
+	bcf	buzz_bit, 0, A
 	
-;try instead of line 120-129?
-	decfsz Alarm_buzz
-	BNN	ALARM
-	BRA	compare_alarm
+	clrf	alarm_buzz, A
 	
-compare_alarm:				;compare alarm
-	btfss	alarm_on, 0		;check if alarm is on
-	return				;return if it isnt on
-	movf	alarm_hrs, W		;otherwise compare clock time to alarm time
-	CPFSEQ	clock_hrs
+    
+Check_Alarm:
+	alarm_setup
+	movlw	0x00
+	cpfseq	alarm_buzz, A
+	bra	Decrement_Alarm_Buzz
+	bra	Compare_Alarm
+
+Decrement_Alarm_Buzz:
+	decf	alarm_buzz, A
+	call	ALARM
 	return
-	movf	alarm_min, W
-	CPFSEQ	clock_min
+	
+Compare_Alarm:  
+	btfss	alarm_on, 0, A
 	return
-	movf	alarm_sec, W
-	CPFSEQ	clock_sec
-	return			    ;return if not the same
+	movf	alarm_hrs, W, A
+	CPFSEQ	clock_hrs, A
+	return
+	movf	alarm_min, W, A
+	CPFSEQ	clock_min, A
+	return
+	movf	alarm_sec, W, A
+	CPFSEQ	clock_sec, A
+	return
 	
 	movlw	0x3C
-	movwf	Alarm_buzz	    ;set alarm_buzz to 60 to be able to buzz for 60 seconds
-	    
-	call ALARM		    ;call alarm if it is the same
+	movwf	alarm_buzz, A
+	
+	call ALARM
 	return
 ALARM:
-	movlw	11000000B	    ;set cursor to first line
-	call	LCD_Set_Position
-	call	Display_ALARM	    ;display alarm while alarm is ringing
-	
-	;call	check_buzz_bit	    ;check the buzz_bit to set it if it was clear and to clear it if it was set
-	BTG	buzz_bit, 0
-	
-	call	buzzer		    ;call buzzer which buzzes when the buzz_bit is set
+	call	LCD_Line2
+	call	Write_ALARM
+
+	BTG	buzz_bit,0
+	call	Buzzer
 
 	return	
-		
-;check_buzz_bit:
-;	btfsc	buzz_bit, 0		;check if buzz bit is set
-;	bra	clear_buzz_bit		;branch to set it if it isnt set
-;	bra	set_buzz_bit		;branch to clear it if it is set
-;clear_buzz_bit:	
-;	bcf	buzz_bit, 0		;clear buzz_bit 
-;	return
-;set_buzz_bit:
-;	bsf	buzz_bit, 0		;set buzz_bit
-;	return
 	
-buzzer:	
+Buzzer:	
 	;Initialize
-	bcf	TRISB, 6		;set RB6 to output
+	bcf	TRISB, 6, A
 	
 	movlw	0x64
-	movwf	buzzer_counter_1	;values for buzzer counter that counts down a second and buzzes at every count
+	movwf	buzzer_counter_1, A
 	movlw	0x1E
-	movwf	buzzer_counter_2	;	"
+	movwf	buzzer_counter_2, A
 
-buzz_loop_1:
-check_cancel_snooze:
+Buzz_Loop_1:
+    
+Check_Cancel_Snooze:
 	call	Keypad
-	movf	keypad_val, W
-	CPFSEQ	hex_C
-	btfss	skip_byte, 0
-	bra	cancel_alarm
-	CPFSEQ	hex_A
-	btfss	skip_byte, 0
-	bra	snooze_alarm
-	
-	call	buzz_loop_2		;call second loop at every count, nested loops
-	movlw	0x1E
-	movwf	buzzer_counter_2	;reset count down value for second loop
-	
-	decfsz	buzzer_counter_1	;decrease till 0 and skip when 0
-	bra	buzz_loop_1
-	return		
-buzz_loop_2:
-	call	buzz_sequence		;buzz at every count
-	decfsz	buzzer_counter_2
-	bra	buzz_loop_2
-	return
+	movf	keypad_val, W, A
+	CPFSEQ	hex_C, A
+	btfss	skip_byte, 0, A
+	bra	Cancel_Alarm
+	CPFSEQ	hex_A, A
+	btfss	skip_byte, 0, A
+	bra	Snooze_Alarm	    
+   
 
-buzz_sequence:	
-check_if_buzz:
-	btfss	buzz_bit, 0
-	bra no_buzz
-	bra yes_buzz
-no_buzz: 
-	call delay_buzzer
-	call delay_buzzer
+	call	Buzz_Loop_2
+	movlw	0x1E
+	movwf	buzzer_counter_2, A
+	
+	decfsz	buzzer_counter_1, A
+	bra	Buzz_Loop_1
 	return
-yes_buzz:	
-	bsf	LATB, 6	;Ouput high
+    
+Buzz_Loop_2:
+	call	Buzz_Sequence
+    
+	decfsz	buzzer_counter_2, A
+	bra	Buzz_Loop_2	
+	return
+	
+	
+	
+Buzz_Sequence:	
+    
+Check_if_Buzz:
+	btfss	buzz_bit, 0, A
+	bra	No_Buzz
+	bra	Yes_Buzz
+	
+No_Buzz:
 	call	delay_buzzer
-	bcf	LATB, 6	;Ouput low
+	call	delay_buzzer
+	return
+	
+Yes_Buzz:	
+	bsf	LATB, 6, A	;Ouput high
+	call	delay_buzzer
+	bcf	LATB, 6, A	;Ouput low
 	call	delay_buzzer
 	return	
-    
-delay_buzzer:
-    movlw   0x20	    ;half the time period long delay
-    call    LCD_delay_x4us
-    return
-
-cancel_alarm:
-	clrf	Alarm_buzz
+	
+	
+	
+Cancel_Alarm:
+	clrf	alarm_buzz, A
 	return
 	
-snooze_alarm:
-	clrf    Alarm_buzz
-	call	Display_snooze
+Snooze_Alarm:
+	clrf	alarm_buzz, A
+	call	Write_Snooze
+	
 	movlw	0x05
-	addwf	alarm_min
+	addwf	alarm_min, A
 	movlw	0x3B
-	CPFSGT	alarm_min
+	cpfsgt	alarm_min, A
 	return
 	movlw	0x3C
-	subwf	alarm_min, 1
-	incf	alarm_hrs
+	subwf	alarm_min, 1, 0	;result stored back in f
+	incf	alarm_hrs, A
 	movlw	0x17
-	CPFSGT	alarm_hrs
+	cpfsgt	alarm_hrs, A
 	return
 	movlw	0x18
-	subwf	alarm_hrs, 1
+	subwf	alarm_hrs, 1, 0
+	
 	return
 
-end
+	
+delay_buzzer:
+	movlw   0x20
+	call    LCD_delay_x4us
+	return
+    
 
 
--- Clock_timer.s
+++ Clock_timer.s
@@ -11,27 +11,28 @@ extrn	Keypad
 
 extrn	Write_Snooze, Write_space, Write_colon, Write_ALARM, Write_Time, Write_Temp
 extrn	LCD_Line2, LCD_Line1
+extrn	alarm_sec, alarm_min, alarm_hrs, alarm, alarm_on, Check_Alarm
     
 global	clock_sec, clock_min, clock_hrs
 global	Clock, Clock_Setup, Rewrite_Clock
 global	hex_A, hex_B, hex_C, hex_D, hex_E, hex_F, hex_null  
-global	alarm_hrs, alarm_min, alarm_sec, Display_Alarm_Time, alarm, alarm_on
+;global	alarm_hrs, alarm_min, alarm_sec, alarm_on
+global	Display_Alarm_Time
     
 psect	udata_acs
 clock_hrs: ds 1
 clock_min: ds 1
 clock_sec: ds 1
     
-alarm_sec:	ds  1
-alarm_min:	ds  1
-alarm_hrs:	ds  1
+;alarm_sec:	ds  1
+;alarm_min:	ds  1
+;alarm_hrs:	ds  1
     
-alarm:	ds 1
-alarm_on:   ds 1
-buzz_bit: ds	1
+;alarm_on:   ds 1
+;buzz_bit: ds	1
     
-buzzer_counter_1: ds 1
-buzzer_counter_2: ds 1
+;buzzer_counter_1: ds 1
+;buzzer_counter_2: ds 1
 
 check_60:	ds  1	;reserving byte to store decimal 60 in hex
 check_24:	ds  1	;reserving byte to store decimal 24 in hex
@@ -47,7 +48,7 @@ hex_E:	ds 1
 hex_F:	ds 1
 hex_null:   	ds  1
 
-alarm_buzz: ds 1    
+;alarm_buzz: ds 1    
     
 skip_byte: ds 1
 
@@ -69,11 +70,10 @@ Clock_Setup:
 	movwf	alarm_min, A
 	movwf	alarm_hrs, A
 	
-	bcf	alarm, 0, A
-	bcf	alarm_on, 0, A
-	bcf	buzz_bit, 0, A
+	;bcf	alarm_on, 0, A
+	;bcf	buzz_bit, 0, A
 	
-	clrf	alarm_buzz, A
+	;clrf	alarm_buzz, A
 	
 	bsf	skip_byte, 0, A
 	
@@ -110,56 +110,6 @@ Clock_Setup:
 	bsf	GIE		; Enable all interrupts
 	
 	return
-    
-Clock:	
-	btfss	TMR0IF		; check that this is timer0 interrupt
-	retfie	f		; if not then return
-	call	Clock_Inc	; increment clock time
-	movff	timer_start_value_1, TMR0H	;setting upper byte timer start value
-	movff	timer_start_value_2, TMR0L		;setting lower byte timer start value
-	bcf	TMR0IF		; clear interrupt flag
-	btfss	operation_check, 0, A ;skip rewrite clock if = 1
-	call	Rewrite_Clock	;write and display clock time as decimal on LCD 
-	call	Check_Alarm
-	retfie	f		; fast return from interrupt	
-	
-Check_Alarm:
-	movlw	0x00
-	cpfseq	alarm_buzz, A
-	bra	Decrement_Alarm_Buzz
-	bra	Compare_Alarm
-
-Decrement_Alarm_Buzz:
-	decf	alarm_buzz, A
-	call	ALARM
-	return
-	
-Compare_Alarm:  
-	btfss	alarm_on, 0, A
-	return
-	movf	alarm_hrs, W, A
-	CPFSEQ	clock_hrs, A
-	return
-	movf	alarm_min, W, A
-	CPFSEQ	clock_min, A
-	return
-	movf	alarm_sec, W, A
-	CPFSEQ	clock_sec, A
-	return
-	
-	movlw	0x3C
-	movwf	alarm_buzz, A
-	
-	call ALARM
-	return
-ALARM:
-	call	LCD_Line2
-	call	Write_ALARM
-
-	BTG	buzz_bit,0
-	call	Buzzer
-
-	return	
 			
 Rewrite_Clock:
 	call	LCD_Line1
@@ -195,106 +145,16 @@ Clock_Inc:
 	return	
 	clrf	clock_hrs, A	    ;set hour time to 0 if = 24
 	return
+	   
+Clock:	
+	btfss	TMR0IF		; check that this is timer0 interrupt
+	retfie	f		; if not then return
+	call	Clock_Inc	; increment clock time
+	movff	timer_start_value_1, TMR0H	;setting upper byte timer start value
+	movff	timer_start_value_2, TMR0L		;setting lower byte timer start value
+	bcf	TMR0IF		; clear interrupt flag
+	btfss	operation_check, 0, A ;skip rewrite clock if = 1
+	call	Rewrite_Clock	;write and display clock time as decimal on LCD 
+	call	Check_Alarm
+	retfie	f		; fast return from interrupt	
 	
-	
-Display_Alarm_Time:
-	movf	alarm_hrs, W, A
-	call Write_Decimal_to_LCD
-	call	Write_colon
-	movf	alarm_min, W, A
-	call Write_Decimal_to_LCD
-	call	Write_colon
-	movf	alarm_sec, W, A
-	call Write_Decimal_to_LCD
-	return
-	  
-Buzzer:	
-	;Initialize
-	bcf	TRISB, 6, A
-	
-	movlw	0x64
-	movwf	buzzer_counter_1, A
-	movlw	0x1E
-	movwf	buzzer_counter_2, A
-
-Buzz_Loop_1:
-    
-Check_Cancel_Snooze:
-	call	Keypad
-	movf	keypad_val, W, A
-	CPFSEQ	hex_C, A
-	btfss	skip_byte, 0, A
-	bra	Cancel_Alarm
-	CPFSEQ	hex_A, A
-	btfss	skip_byte, 0, A
-	bra	Snooze_Alarm	    
-   
-
-	call	Buzz_Loop_2
-	movlw	0x1E
-	movwf	buzzer_counter_2, A
-	
-	decfsz	buzzer_counter_1, A
-	bra	Buzz_Loop_1
-	return
-    
-Buzz_Loop_2:
-	call	Buzz_Sequence
-    
-	decfsz	buzzer_counter_2, A
-	bra	Buzz_Loop_2	
-	return
-	
-	
-	
-Buzz_Sequence:	
-    
-Check_if_Buzz:
-	btfss	buzz_bit, 0, A
-	bra	No_Buzz
-	bra	Yes_Buzz
-	
-No_Buzz:
-	call	delay_buzzer
-	call	delay_buzzer
-	return
-	
-Yes_Buzz:	
-	bsf	LATB, 6, A	;Ouput high
-	call	delay_buzzer
-	bcf	LATB, 6, A	;Ouput low
-	call	delay_buzzer
-	return	
-	
-	
-	
-Cancel_Alarm:
-	clrf	alarm_buzz, A
-	return
-	
-Snooze_Alarm:
-	clrf	alarm_buzz, A
-	call	Write_Snooze
-	
-	movlw	0x05
-	addwf	alarm_min, A
-	movlw	0x3B
-	cpfsgt	alarm_min, A
-	return
-	movlw	0x3C
-	subwf	alarm_min, 1, 0	;result stored back in f
-	incf	alarm_hrs, A
-	movlw	0x17
-	cpfsgt	alarm_hrs, A
-	return
-	movlw	0x18
-	subwf	alarm_hrs, 1, 0
-	
-	return
-
-	
-delay_buzzer:
-	movlw   0x20
-	call    LCD_delay_x4us
-	return
-    
